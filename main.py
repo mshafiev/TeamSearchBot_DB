b7b232b51f72c6b46a5f56b90b49c7541961648f
mikhail shafiev <m_shafiev@list.ru>
2025-07-31 14:01:10 +0300

    format

--- main.py
+++ main.py
@@ -9,32 +9,39 @@ from typing import Optional
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 class OlympsBase(BaseModel):
-    name: str  
-    profile: str  
+    name: str
+    profile: str
     subject: str
     level: int  # 1,2,3, 0-не рсош
     link: str
 
+
 class OlympResultBase(BaseModel):
     olymp: OlympsBase
-    result: int # 0-победитель, 1-призер, 2-финалист, 3-участник
+    result: int  # 0-победитель, 1-призер, 2-финалист, 3-участник
     year: str
     tg_id: int
+
 
 class UsersBase(BaseModel):
     tg_id: int
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     middle_name: Optional[str] = None
-    phone: Optional[str] = None
+    phone: Optional[str] = NoneÒ
     phone_verified: Optional[bool] = False  # поле для верификации телефона
     age: Optional[int] = None
     city: Optional[str] = None
     status: Optional[int] = None  # 0-свободен / 1-в отношениях
-    goal: Optional[int] = None  # 0-совместный бот, 1-общение, 2-поиск команды, 3-отношения
+    goal: Optional[int] = (
+        None  # 0-совместный бот, 1-общение, 2-поиск команды, 3-отношения
+    )
     who_interested: Optional[int] = None  # 0-ж / 1-м / 2-все
-    date_of_birth: Optional[str] = None  # дата рождения пользователя (в формате ДД-ММ-ГГГГ)
+    date_of_birth: Optional[str] = (
+        None  # дата рождения пользователя (в формате ДД-ММ-ГГГГ)
+    )
 
 
 def get_db():
@@ -44,44 +51,43 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get('/olymp/{olymp_id}')
+@app.get("/olymp/{olymp_id}")
 async def read_olymp(olymp_id: int, db: db_dependency):
     result = db.query(models.Olymps).filter(models.Olymps.id == olymp_id).first()
     if not result:
-        raise HTTPException(status_code=404, detail='Olymp is not found')
+        raise HTTPException(status_code=404, detail="Olymp is not found")
     return result
 
 
-@app.post('/olymp/create/')
+@app.post("/olymp/create/")
 async def create_olymp(olymp: OlympsBase, db: db_dependency):
     db_olymp = models.Olymps(
         name=olymp.name,
         profile=olymp.profile,
         subject=olymp.subject,
         level=olymp.level,
-        link=olymp.link
+        link=olymp.link,
     )
     db.add(db_olymp)
     db.commit()
     db.refresh(db_olymp)
 
 
-@app.delete('/olymp/delete/{olymp_id}')
+@app.delete("/olymp/delete/{olymp_id}")
 async def delete_olymp(olymp_id: int, db: db_dependency):
     olymp = db.query(models.Olymps).filter(models.Olymps.id == olymp_id).first()
     if not olymp:
-        raise HTTPException(status_code=404, detail='Олимпиада не найдена')
+        raise HTTPException(status_code=404, detail="Олимпиада не найдена")
     db.delete(olymp)
     db.commit()
     return {"detail": f"Олимпиада с id {olymp_id} успешно удалена"}
 
 
-
-
-@app.post('/user/create/')
+@app.post("/user/create/")
 async def create_user(tg_id: int, db: db_dependency):
     """
     Создаёт нового пользователя по tg_id.
@@ -89,7 +95,9 @@ async def create_user(tg_id: int, db: db_dependency):
     """
     existing_user = db.query(models.Users).filter(models.Users.tg_id == tg_id).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="Пользователь с таким tg_id уже существует")
+        raise HTTPException(
+            status_code=400, detail="Пользователь с таким tg_id уже существует"
+        )
     new_user = models.Users(tg_id=tg_id)
     db.add(new_user)
     db.commit()
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base
 
+
 class Olymps(Base):
-    __tablename__ = 'olymps'
+    __tablename__ = "olymps"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
@@ -12,22 +13,23 @@ class Olymps(Base):
     link = Column(String, nullable=True)
 
 
-# Модель для результатов олимпиад
 class OlympResult(Base):
-    __tablename__ = 'olymp_results'
+    __tablename__ = "olymp_results"
 
     id = Column(Integer, primary_key=True, index=True)
-    olymp_id = Column(Integer, ForeignKey('olymps.id'), nullable=False, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    result = Column(Integer, nullable=False)  # 0-победитель, 1-призер, 2-финалист, 3-участник
+    olymp_id = Column(Integer, ForeignKey("olymps.id"), nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    result = Column(
+        Integer, nullable=False
+    )  # 0-победитель, 1-призер, 2-финалист, 3-участник
     year = Column(String, nullable=False)
 
-# Модель для пользователей
+
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    tg_id = Column(Integer, primary_key=True,unique=True, nullable=False, index=True)
+    tg_id = Column(Integer, primary_key=True, unique=True, nullable=False, index=True)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
     middle_name = Column(String, nullable=True)
@@ -36,6 +38,10 @@ class Users(Base):
     age = Column(Integer, nullable=True)
     city = Column(String, nullable=True)
     status = Column(Integer, nullable=True)  # 0-свободен / 1-в отношениях
-    goal = Column(Integer, nullable=True)  # 0-совместный бот, 1-общение, 2-поиск команды, 3-отношения
+    goal = Column(
+        Integer, nullable=True
+    )  # 0-совместный бот, 1-общение, 2-поиск команды, 3-отношения
     who_interested = Column(Integer, nullable=True)  # 0-ж / 1-м / 2-все
-    date_of_birth = Column(String, nullable=True)  # дата рождения пользователя (в формате ДД-ММ-ГГГГ)
+    date_of_birth = Column(
+        String, nullable=True
+    )  # дата рождения пользователя (в формате ДД-ММ-ГГГГ)
